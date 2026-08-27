2ae718fbcde37c736ba54ba2f4b037486da3c667
SeascapeCommerce <144998055+SeascapeCommerce@users.noreply.github.com>
2026-08-27 16:12:05 -0700

    Fix syntax error in gsap.registerPlugin call

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,5 @@
 // Register ONLY the plugins you actually loaded from the CDN.
-gsap.registerPlugin(ScrollTrigger, Draggable,);
+gsap.registerPlugin(ScrollTrigger, Draggable);
 
 const phones = gsap.utils.toArray(".draggable-phone");
 
