60babc72515ecdd2b45e2f01085ffb7a60c4acd1
SeascapeCommerce <144998055+SeascapeCommerce@users.noreply.github.com>
2026-08-30 17:20:55 -0700

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -385,3 +385,19 @@ gsap.from(".showcase-card",{
   }
 
 });
+
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    scrub: 1,
+    pin: true,
+    trigger: "#pin-windmill",
+    start: "50% 50%",
+    endTrigger: "#pin-windmill-wrap",
+    end: "bottom 50%",
+  },
+});
+
+tl.to("#pin-windmill-svg", {
+  rotateZ: 900,
+});
